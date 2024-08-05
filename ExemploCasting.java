849723df77bcf3c315e86cafb99b50e597f8a093
Tiago Eich <tiagobackeseicheich@gmail.com>
2024-08-05 09:49:05 -0300

    Add files via upload

---
+++ ExemploCasting.java
@@ -0,0 +1,11 @@
+public class ExemploCasting {
+    public static void main(String[] args) {
+        double numero1 = 5.45;
+        int numero2 = 7;
+        int soma = (int) numero1 + numero2;
+        System.out.println(soma);
+    }
+}
+
+/* Declare uma variável do tipo double e uma variável do tipo int.
+ Faça o casting da variável double para int e imprima o resultado. */
