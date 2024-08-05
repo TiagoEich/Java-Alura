1e70fb7aee6e71f81ce424ec0bcbea5d4c9722da
Tiago Eich <tiagobackeseicheich@gmail.com>
2024-08-05 09:50:54 -0300

    Delete Media.java

--- Media.java
+++
@@ -1,8 +0,0 @@
-public class Media {
-    public static void main(String[] args) {
-        double nota1 = 7.8;
-        double nota2 = 6.4;
-        System.out.println((nota1 + nota2)/2);
-    }
-}
-//Crie um programa que realize a média de duas notas decimais e exiba o resultado.
